6dd0fc0c4d1136d529534027e29dd26b38656e7c
asdf1163 <rafaszromek@gmail.com>
2022-01-20 12:36:31 +0100

    dev

--- src/components/workflow/Workflow.tsx
+++ src/components/workflow/Workflow.tsx
@@ -3,12 +3,20 @@ import React, { useEffect, useRef, useState } from "react";
 const Workflow = () => {
   interface Iword {
     [x: string]: any;
-    splitedWord: string | string[]| object | object[];
+    splitedWord: string | string[] | object | object[];
     correct: boolean | null;
   }
 
+  interface IcurrentWordID {
+    word: number;
+    sign: number;
+  }
+
   const [word, setWord] = useState<Iword[]>([]);
-  const [currentWordID, setCurrentWordID] = useState<number[]>([0, 0]);
+  const [currentWordID, setCurrentWordID] = useState<IcurrentWordID>({
+    word: 0,
+    sign: 0,
+  });
   const positionCurrent = useRef<HTMLSpanElement>(null);
 
   const data =
@@ -17,13 +25,24 @@ const Workflow = () => {
   const dataSplit = data.split(" ");
 
   const correctKeyPress = (event: any) => {
-    console.log(word[0][2].splitedWord);
     const tempArray = [...word];
-    
+
     if (event.keyCode === 8 || event.keyCode === 46) {
-      word[currentWordID[0]][currentWordID[1]].correct = null;
-      // currentWordID[0] !== 0 && currentWordID[1] !== 0 && setCurrentWordID((state) => );
-      return setWord(tempArray);
+      if (currentWordID.word === 0 && currentWordID.sign === 0) return 0;
+      else {
+        word[currentWordID.word][currentWordID.sign].correct = null;
+        if (currentWordID.sign !== 0)
+          setCurrentWordID({
+            word: currentWordID.word,
+            sign: currentWordID.sign - 1,
+          });
+        else
+          setCurrentWordID({
+            word: currentWordID.word - 1,
+            sign: word[currentWordID.word-1].length-1,
+          });
+        return setWord(tempArray);
+      }
     }
 
     if (
@@ -31,19 +50,31 @@ const Workflow = () => {
       event.keyCode === 32 ||
       event.keyCode === 49
     ) {
-      if (word[currentWordID[0]][currentWordID[1]].splitedWord === event.key) {
-        // word[currentWordID].correct = true;
+      if (
+        word[currentWordID.word][currentWordID.sign].splitedWord === event.key
+      ) {
+        tempArray[currentWordID.word][currentWordID.sign].correct = true;
         setWord(tempArray);
-        // setCurrentWordID((state) => state + 1);
+        if (currentWordID.sign !== word[currentWordID.word].length-1)
+          setCurrentWordID({
+            word: currentWordID.word,
+            sign: currentWordID.sign + 1,
+          });
+        else setCurrentWordID({ word: currentWordID.word + 1, sign: 0 });
       } else {
-        // word[currentWordID].correct = false;
+        tempArray[currentWordID.word][currentWordID.sign].correct = false;
         setWord(tempArray);
-        // setCurrentWordID((state) => state + 1);
+        if (currentWordID.sign !== word[currentWordID.word].length-1 )
+          setCurrentWordID({
+            word: currentWordID.word,
+            sign: currentWordID.sign + 1,
+          });
+        else setCurrentWordID({ word: currentWordID.word + 1, sign: 0 });
       }
     }
   };
-
   const space = "\u00A0";
+
   useEffect(() => {
     for (const dataSign of dataSplit) {
       const splitedData = dataSign.split("");
@@ -68,11 +99,12 @@ const Workflow = () => {
           style={{ transform: `translate(0, -${4}px)` }}
           className="workflow__container"
         >
-          {word.map((state, index): JSX.Element => {
+          {console.log(currentWordID)}
+          {word.map((state, indexWord): JSX.Element => {
             return (
-              <div key={`word${index}`}>
-                {state.map((character:any, index:number): JSX.Element => {
-                  return currentWordID[0] === index ? (
+              <div key={`word${indexWord}`}>
+                {state.map((character: any, index: number): JSX.Element => {
+                  return currentWordID.word === indexWord && currentWordID.sign === index ? (
                     <span
                       key={`sign${index}`}
                       className="workflow__sign workflow__sign--current"
@@ -80,11 +112,11 @@ const Workflow = () => {
                     >
                       {character.splitedWord}
                     </span>
-                  ) : state.correct === null ? (
+                  ) : character.correct === null ? (
                     <span key={`sign${index}`} className="workflow__sign">
                       {character.splitedWord}
                     </span>
-                  ) : state.correct === true ? (
+                  ) : character.correct === true ? (
                     <span
                       key={`sign${index}`}
                       className="workflow__sign workflow__sign--correct"
